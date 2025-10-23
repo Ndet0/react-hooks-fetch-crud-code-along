fc2fd79850a2fe291a4bd9a15f5ee29b454c3ba0
Ndet0 <festusndet00@gmail.com>
2025-10-23 10:45:51 +0300

    Adds App.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+  function handleAddToCart() {
+    const updatedItem = { ...item, isInCart: !item.isInCart };
+    onUpdateItem(updatedItem);
+  }
+
+  function handleDelete() {
+    onDeleteItem(item.id);
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"} onClick={handleAddToCart}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,8 +7,46 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((data) => setItems(data));
+  }, []);
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
+  }
+
+  function addItem(newItem) {
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newItem),
+    })
+      .then((r) => r.json())
+      .then((item) => setItems([...items, item]));
+  }
+
+  function updateItem(updatedItem) {
+    fetch(`http://localhost:4000/items/${updatedItem.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedItem),
+    })
+      .then((r) => r.json())
+      .then((item) => {
+        const updatedItems = items.map((i) => (i.id === item.id ? item : i));
+        setItems(updatedItems);
+      });
+  }
+
+  function deleteItem(id) {
+    fetch(`http://localhost:4000/items/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      const updatedItems = items.filter((i) => i.id !== id);
+      setItems(updatedItems);
+    });
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,14 +57,19 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={addItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={updateItem}
+            onDeleteItem={deleteItem}
+          />
         ))}
       </ul>
     </div>
